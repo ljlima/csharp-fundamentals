023dad89fdf7d69dfc011549a7b7a76561a9661a
Jean Lima <116501375+ljlima@users.noreply.github.com>
2024-05-16 00:35:48 -0400

    Update
    added functionalities:
    
    * Dictionary <string, int>: atribution and search keys
    * Try-catch exception
    * Foreach feature
    * String features: Add(), ToUpper(), Average()
    * Console features: Clear(), ReadKey()
    * Thread features: Sleep()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿void ExibirMensagemBoasVindas()
+﻿Dictionary<String,List<int>> bandas = new Dictionary<string, List<int>>();
+void ExibirMensagemBoasVindas()
 {
     string mensagemBoasVindas = "Bem vindo ao Screen Audio!";
     //verbatim literal ; @" string desejada"  
@@ -15,11 +16,11 @@
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
 ");
     Console.WriteLine(mensagemBoasVindas);
-
 }
 
 void ExibirMenuOpcoes()
 {
+    ExibirMensagemBoasVindas();
     // "\n" dá o espaço
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para exibir todas as bandas");
@@ -33,28 +34,116 @@ void ExibirMenuOpcoes()
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida); // converte para inteiro
     switch (opcaoEscolhidaNumerica)
     {
-        case 1:
-            Console.WriteLine("Você digitou a opção: " + opcaoEscolhidaNumerica);
+        case 1:  RegistrarBanda();
             break;
-        case 2:
-            Console.WriteLine("Você digitou a opção: " + opcaoEscolhidaNumerica);
+        case 2: VisualizarBanda();
             break;
-        case 3:
-            Console.WriteLine("Você digitou a opção: " + opcaoEscolhidaNumerica);
+        case 3: AvaliarBanda();
             break;
-        case 4:
-            Console.WriteLine("Você digitou a opção: " + opcaoEscolhidaNumerica);
+        case 4: VerMediaBanda();
             break;
 
         case -1:
             Console.WriteLine("tchau :)");
             break;
-
         default:
             Console.WriteLine("Opção inválida!");
             break;
     }
 }
 
-ExibirMensagemBoasVindas();
+void RegistrarBanda()
+{
+    Console.Clear();
+    System.Console.WriteLine("Digite o nome da banda que deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    try
+    {
+        bandas.Add(nomeBanda.ToUpper(),new List<int>());
+        System.Console.WriteLine($"Banda {nomeBanda} foi adicionada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirMenuOpcoes();
+    }
+    catch (System.Exception)
+    {
+
+        throw;
+    }
+    
+}
+
+void VisualizarBanda()
+{
+    Console.Clear();
+    System.Console.WriteLine("Foram registradas até o momento as bandas: ");
+    foreach (var banda in bandas.Keys)
+        {
+            System.Console.WriteLine(banda);
+        }
+    Thread.Sleep(2000);
+
+
+    Console.Clear();
+    ExibirMenuOpcoes();
+
+}
+
+void AvaliarBanda()
+{
+    Console.Clear();
+    System.Console.WriteLine("Digite o nome da banda que deseja avaliar:");
+    try
+    {
+        string bandaAvaliada = Console.ReadLine()!;
+        bandaAvaliada = bandaAvaliada.ToUpper();
+
+        if (bandas.ContainsKey(bandaAvaliada))
+        {
+            System.Console.WriteLine("Qual a nota dada a banda?");
+            int nota = int.Parse(Console.ReadLine()!);
+            bandas[bandaAvaliada].Add(nota);
+            System.Console.WriteLine($"Nota {nota} atribuída a banda {bandaAvaliada.ToUpper()} com sucesso!");
+            Thread.Sleep(2000);
+        }
+        else
+        {
+            System.Console.WriteLine("Não foi possível encontrar a banda digitada!!");
+            System.Console.WriteLine("Pressione uma tecla para voltar no menu inicial: ");
+            Console.ReadKey();
+
+        }
+        Console.Clear();
+        ExibirMenuOpcoes();
+    }
+    catch (System.Exception)
+    {
+        
+        throw;
+    }  
+}
+
+void VerMediaBanda()
+{
+    Console.Clear();
+    System.Console.WriteLine("Digite o nome da banda que deseja avaliar:");
+    string bandaAvaliada = Console.ReadLine()!;
+    bandaAvaliada = bandaAvaliada.ToUpper();
+    if (bandas.ContainsKey(bandaAvaliada))
+    {
+        List<int> notasBanda = bandas[bandaAvaliada];
+        System.Console.WriteLine($"A média de notas para a banda {bandaAvaliada} é {notasBanda.Average()}");
+        Thread.Sleep(2000);
+    }
+    else
+    {
+        System.Console.WriteLine("Não foi possível encontrar a banda digitada!!");
+        System.Console.WriteLine("Pressione uma tecla para voltar no menu inicial: ");
+        Console.ReadKey();
+
+    }
+    Console.Clear();
+    ExibirMenuOpcoes();
+}
+
 ExibirMenuOpcoes();
