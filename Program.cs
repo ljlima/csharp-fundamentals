da4048d1c6e129cd403e139ef82064d7376e236d
Jean Lima <116501375+ljlima@users.noreply.github.com>
2024-06-19 01:38:37 -0400

    update

--- Program.cs
+++ Program.cs
@@ -15,6 +15,19 @@ using (HttpClient client = new HttpClient())
         //LinqOrder.ExibirListaArtistasOrdenada(musicas);
         ///LinqFilter.FiltrarArtistaPorGenero(musicas, "blues");
         LinqFilter.FiltrarMusicaPorArtista(musicas, "U2");
+
+
+        MusicasPreferidas musicasPreferidasJao = new MusicasPreferidas("jao");
+        musicasPreferidasJao.AdicionarMusicasPreferidas(musicas[66]);
+        musicasPreferidasJao.AdicionarMusicasPreferidas(musicas[777]);
+        musicasPreferidasJao.AdicionarMusicasPreferidas(musicas[88]);
+        musicasPreferidasJao.AdicionarMusicasPreferidas(musicas[11]);
+        musicasPreferidasJao.AdicionarMusicasPreferidas(musicas[5]);
+
+        musicasPreferidasJao.ExibirMusicasPreferidas();
+        musicasPreferidasJao.CriarArquivoJson();
+
+        musicasPreferidasJao.CriarArquivosTxt();
     }
     catch (System.Exception e)
     {
